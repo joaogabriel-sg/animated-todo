8b9ee3d8d71de224314cdadcf472801e9bd357e0
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-03 16:56:55 -0300

    :sparkles: add a handle sign in function when click in the login button

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 
+import { useAuth } from '../../hooks/useAuth';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
 export function Login() {
+	const { signIn } = useAuth();
 	const [name, setName] = useState('');
 
 	const formattedNameLength = name.trim().length;
 	const containerOpacity = formattedNameLength / 4 - 1;
+
+	function handleSignInUser() {
+		const formattedName = name.trim();
+		if (formattedName === '') return;
+
+		const doesTheNameHaveANumber = !!formattedName.match(/\d/g);
+		if (doesTheNameHaveANumber) return;
+
+		signIn(name);
+	}
 
 	return (
 		<Container containerOpacity={containerOpacity}>
@@ -23,7 +36,9 @@ export function Login() {
 					onChange={({ target }) => setName(target.value)}
 					placeholder="Digite seu nome para entrar"
 				/>
-				<button type="button">Entrar no to.DO</button>
+				<button type="button" onClick={handleSignInUser}>
+					Entrar no to.DO
+				</button>
 			</div>
 		</Container>
 	);
