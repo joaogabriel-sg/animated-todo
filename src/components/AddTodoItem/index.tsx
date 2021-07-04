b99843a7e248f8190d726070e42e20772cfcf2ef
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-04 13:47:20 -0300

    :sparkles: add a handle add new todo item to add todo item component

--- src/components/AddTodoItem/index.tsx
+++ src/components/AddTodoItem/index.tsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import { FiPlus } from 'react-icons/fi';
+
+import { useTodo } from '../../hooks/useTodo';
 
 import { Container } from './styles';
 
 export function AddTodoItem() {
+	const { addNewTodo } = useTodo();
+	const [title, setTitle] = useState('');
+
+	function handleAddNewTodoItem() {
+		const formattedTitle = title.trim();
+		if (formattedTitle === '') return;
+
+		addNewTodo(formattedTitle);
+		setTitle('');
+	}
+
 	return (
 		<Container>
-			<input type="text" placeholder="Digite seu nome para entrar" />
-			<button type="button">
+			<input
+				type="text"
+				value={title}
+				onChange={({ target }) => setTitle(target.value)}
+				placeholder="Digite seu nome para entrar"
+			/>
+			<button
+				type="button"
+				onClick={handleAddNewTodoItem}
+				disabled={title.length === 0}
+			>
 				<FiPlus size={32} />
 			</button>
 		</Container>
