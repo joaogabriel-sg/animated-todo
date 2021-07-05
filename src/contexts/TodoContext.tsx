704b5e11cf21c963bd8839399b27ee140a991436
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-05 09:07:07 -0300

    :sparkles: add edit todo item functionalities

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -2,6 +2,7 @@ import { createContext, PropsWithChildren, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { useAuth } from '../hooks/useAuth';
+import { useModal } from '../hooks/useModal';
 
 interface Todo {
 	id: string;
@@ -11,15 +12,20 @@ interface Todo {
 
 interface TodoContextProps {
 	todos: Todo[];
+	todoToEdit: Todo;
 	addNewTodo: (title: string) => void;
 	deleteTodoItem: (id: string) => void;
+	editTodoItem: (todo: Todo) => void;
+	openTodoItemInEditMode: (todo: Todo) => void;
 }
 
 export const TodoContext = createContext({} as TodoContextProps);
 
 export function TodoProvider({ children }: PropsWithChildren<unknown>) {
 	const { data } = useAuth();
+	const { openModal, closeModal } = useModal();
 	const [todos, setTodos] = useState(data.todos);
+	const [todoToEdit, setTodoToEdit] = useState({} as Todo);
 
 	function addNewTodo(title: string) {
 		const formattedTitle = title.trim();
@@ -53,8 +59,39 @@ export function TodoProvider({ children }: PropsWithChildren<unknown>) {
 		});
 	}
 
+	function editTodoItem(newTodo: Todo) {
+		setTodos((oldTodos) => {
+			const newTodoList = oldTodos.map((oldTodo) => {
+				if (oldTodo.id === newTodo.id) return newTodo;
+				return oldTodo;
+			});
+
+			localStorage.setItem(
+				'@AnimatedTodo:user',
+				JSON.stringify({ ...data, todos: newTodoList }),
+			);
+			return newTodoList;
+		});
+
+		closeModal();
+	}
+
+	function openTodoItemInEditMode(todo: Todo) {
+		openModal();
+		setTodoToEdit(todo);
+	}
+
 	return (
-		<TodoContext.Provider value={{ todos, addNewTodo, deleteTodoItem }}>
+		<TodoContext.Provider
+			value={{
+				todos,
+				todoToEdit,
+				addNewTodo,
+				deleteTodoItem,
+				editTodoItem,
+				openTodoItemInEditMode,
+			}}
+		>
 			{children}
 		</TodoContext.Provider>
 	);
