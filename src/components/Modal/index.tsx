4ae997a6ffd313ccbb53aed7f99d17c4233d459b
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-05 09:10:07 -0300

    :sparkles: add edit mode and update functionalities

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,14 +1,16 @@
-import { useCallback } from 'react';
-import { useRef, useEffect } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 import { FaEdit } from 'react-icons/fa';
 
 import { useModal } from '../../hooks/useModal';
+import { useTodo } from '../../hooks/useTodo';
 
 import { Container } from './styles';
 
 export function Modal() {
 	const { isModalOpen, closeModal } = useModal();
+	const { todoToEdit, editTodoItem } = useTodo();
 	const overlayRef = useRef(null);
+	const [title, setTitle] = useState('');
 
 	const handleCloseModal = useCallback(() => {
 		closeModal();
@@ -28,6 +30,13 @@ export function Modal() {
 		};
 	}, [handleCloseModal, isModalOpen]);
 
+	function handleEditTodoItem() {
+		const newTodoItem = { ...todoToEdit, title };
+
+		editTodoItem(newTodoItem);
+		setTitle('');
+	}
+
 	if (!isModalOpen) return null;
 
 	return (
@@ -37,10 +46,22 @@ export function Modal() {
 					<FaEdit size={32} />
 					<h2>Editar ToDo</h2>
 				</header>
-				<label htmlFor="todo">ToDo:</label>
-				<input type="text" id="todo" />
+				<label htmlFor="todo">ToDo: {todoToEdit.title}</label>
+				<input
+					type="text"
+					id="todo"
+					value={title}
+					onChange={({ target }) => setTitle(target.value)}
+					placeholder="Digite o novo título da ToDo"
+				/>
 				<div>
-					<button type="button">Confirmar</button>
+					<button
+						type="button"
+						onClick={handleEditTodoItem}
+						disabled={title === ''}
+					>
+						Confirmar
+					</button>
 					<button type="button" onClick={handleCloseModal}>
 						Cancelar
 					</button>
