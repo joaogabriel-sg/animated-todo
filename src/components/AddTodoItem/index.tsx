142fe37e81d6ad531a5754bf14d37bedb4a90bd1
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-07 16:21:42 -0300

    :sparkles: add motion animation in add todo item component

--- src/components/AddTodoItem/index.tsx
+++ src/components/AddTodoItem/index.tsx
@@ -1,9 +1,40 @@
 import { useState } from 'react';
 import { FiPlus } from 'react-icons/fi';
+import { motion } from 'framer-motion';
 
 import { useTodo } from '../../hooks/useTodo';
 
 import { Container } from './styles';
+
+const inputVariants = {
+	hidden: {
+		opacity: 0,
+		y: 50,
+	},
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 1,
+			delay: 2,
+		},
+	},
+};
+
+const buttonVariants = {
+	hidden: {
+		opacity: 0,
+		scale: 0,
+	},
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			duration: 1,
+			delay: 2.5,
+		},
+	},
+};
 
 export function AddTodoItem() {
 	const { addNewTodo } = useTodo();
@@ -19,19 +50,25 @@ export function AddTodoItem() {
 
 	return (
 		<Container>
-			<input
+			<motion.input
+				variants={inputVariants}
+				initial="hidden"
+				animate="visible"
 				type="text"
 				value={title}
 				onChange={({ target }) => setTitle(target.value)}
 				placeholder="Digite o título da ToDo"
 			/>
-			<button
+			<motion.button
+				variants={buttonVariants}
+				initial="hidden"
+				animate="visible"
 				type="button"
 				onClick={handleAddNewTodoItem}
 				disabled={title.length === 0}
 			>
 				<FiPlus size={32} />
-			</button>
+			</motion.button>
 		</Container>
 	);
 }
