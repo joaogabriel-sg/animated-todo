670dcc3a45b40b84bbe507a54256c97a3aba885b
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-07 10:23:56 -0300

    :sparkles: add toggle theme function in the header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+import { useTheme } from '../../hooks/useTheme';
 
 import logoWhiteImg from '../../assets/logo-white.svg';
 
@@ -7,13 +9,23 @@ import { UserOptions } from '../UserOptions';
 import { Container } from './styles';
 
 export function Header() {
+	const { theme, toggleTheme } = useTheme();
+
+	function handleToggleTheme() {
+		toggleTheme();
+	}
+
 	return (
 		<Container>
 			<div>
 				<img src={logoWhiteImg} alt="to.DO" />
 				<div>
-					<button type="button">
-						<FaMoon size={32} />
+					<button type="button" onClick={handleToggleTheme}>
+						{theme.name === 'light' ? (
+							<FaMoon size={32} />
+						) : (
+							<FaSun size={32} />
+						)}
 					</button>
 					<UserOptions />
 				</div>
